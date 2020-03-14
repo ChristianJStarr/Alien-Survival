8d1775871d0b53c54f58f7e27ef60ad3fec88111
ChristianJStarr <56407604+ChristianJStarr@users.noreply.github.com>
2020-03-14 09:04:32 -0400

    Changed to landscape UI

--- Assets/Scenes/MainMenu/MainMenuScript.cs
+++ Assets/Scenes/MainMenu/MainMenuScript.cs
@@ -10,23 +10,61 @@ public class MainMenuScript : MonoBehaviour
     public GameObject profileMenu;
     public GameObject settingsMenu;
     public GameObject onlineMenu;
+    public Camera cam;
+    static Transform camReset;
+    static Transform playTarget;
+    public Vector3 playTargetCord;
+    public Quaternion playTargetRot;
+    
+    Vector3 resetTargetCord;
+    Quaternion resetTargetRot;
+    Vector3 camTargetposition;
+    Quaternion camTargetrotation;
 
+    void Start() 
+    {
 
-
+        camReset = cam.transform;
+        resetTargetCord = camReset.position;
+        resetTargetRot = camReset.localRotation;
+        camTargetposition = resetTargetCord;
+        camTargetrotation = resetTargetRot;
+    }
+    void Update()
+    {
+        float step = 15 * Time.deltaTime;
+        float step2 = 15 * Time.deltaTime;
+        if (cam.transform.position != camTargetposition) 
+        {
+            Debug.Log("2");
+            cam.transform.position = Vector3.MoveTowards(cam.transform.position, camTargetposition,step);
+        }
+        if (cam.transform.localRotation.y != camTargetrotation.y)
+        {
+            cam.transform.localRotation = Quaternion.RotateTowards(cam.transform.localRotation, camTargetrotation, step2);
+            Debug.Log(cam.transform.localRotation + "   :   " + camTargetrotation);
+        }
+    }
     public void PlayMenu() 
     {
         mainScreen.SetActive(false);
         onlineMenu.SetActive(true);
+        camTargetposition = playTargetCord;
+        camTargetrotation = playTargetRot;
     }
     public void ProfileMenu() 
     {
         mainScreen.SetActive(false);
         profileMenu.SetActive(true);
+        camTargetposition = playTargetCord;
+        camTargetrotation = playTargetRot;
     }
     public void SettingsMenu() 
     {
         mainScreen.SetActive(false);
         settingsMenu.SetActive(true);
+        camTargetposition = playTargetCord;
+        camTargetrotation = playTargetRot;
     }
     public void CloseMenu() 
     {
@@ -39,6 +77,8 @@ public class MainMenuScript : MonoBehaviour
 
             mainScreen.SetActive(true);
 
+        camTargetposition = resetTargetCord;
+        camTargetrotation = resetTargetRot;
     }
     public void JoinServer()
     {
---
+++ Assets/Scenes/MainMenu/UfoMove.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UfoMove : MonoBehaviour
+{
+    Vector3 target;
+    Transform ufo;
+    Vector3 reset;
+    public int speed = 80;
+    bool sendUFO;
+    public float ufoDelay = 15;
+
+    void Start()
+    {
+        ufo = GetComponent<Transform>();
+        reset = ufo.position;
+        target = new Vector3(reset.x - 200, reset.y, reset.z);
+        StartCoroutine(WaitForUFO());
+    }
+
+    void Update()
+    {
+        if (ufo.position != target && sendUFO == true) 
+        {
+            float step = speed * Time.deltaTime;
+            ufo.position = Vector3.MoveTowards(ufo.position, target, step);
+        }
+        else if(sendUFO == true)
+        {
+            sendUFO = false;
+            ufo.position = reset;
+            StartCoroutine(WaitForUFO());
+        }
+    }
+    IEnumerator WaitForUFO() 
+    {
+
+        yield return new WaitForSeconds(ufoDelay);
+        sendUFO = true;
+    }
+}   
