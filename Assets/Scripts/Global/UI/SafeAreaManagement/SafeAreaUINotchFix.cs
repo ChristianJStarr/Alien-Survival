8d62452385f155d7ad9d2e795abfd76410a3c342
Christian Starr <56407604+ChristianJStarr@users.noreply.github.com>
2020-10-07 10:36:00 -0400

    Safe area fix

--- Assets/Scripts/Global/UI/SafeAreaManagement/SafeAreaUINotchFix.cs
+++ Assets/Scripts/Global/UI/SafeAreaManagement/SafeAreaUINotchFix.cs
@@ -6,24 +6,53 @@ public class SafeAreaUINotchFix : MonoBehaviour
 {
     private RectTransform _rectTransform;
 
-    private bool flattenSide = true;
+    private bool flattenSide = false;
 
-    private void Awake()
+    private void Start()
     {
+        string[] errorNotchDevices = { "iPhone10,3", "iPhone10,6", "iPhone11,8", "iPhone11,2", "iPhone11,6", "iPhone11,4", "iPhone12,1", "iPhone12,3", "iPhone12,5" };
+
+
+
+
+
+
+
+
+
+
+
         _rectTransform = GetComponent<RectTransform>();
         //Find devices with fucked up notch safe areas.
-        if (UnityEngine.iOS.Device.generation == UnityEngine.iOS.DeviceGeneration.iPhone11ProMax 
-            || UnityEngine.iOS.Device.generation == UnityEngine.iOS.DeviceGeneration.iPhone11Pro
-            || UnityEngine.iOS.Device.generation == UnityEngine.iOS.DeviceGeneration.iPhone11 
-            || UnityEngine.iOS.Device.generation == UnityEngine.iOS.DeviceGeneration.iPhoneX 
-            || UnityEngine.iOS.Device.generation == UnityEngine.iOS.DeviceGeneration.iPhoneXR 
-            || UnityEngine.iOS.Device.generation == UnityEngine.iOS.DeviceGeneration.iPhoneXS
-            || UnityEngine.iOS.Device.generation == UnityEngine.iOS.DeviceGeneration.iPhoneXSMax) 
+
+        if (UnityEngine.iOS.Device.generation.ToString() != "Unknown") 
         {
-            flattenSide = true;
-            DebugMsg.Notify("Notched Iphone Detected. Fixing Inventory UI", 3);
+            if (UnityEngine.iOS.Device.generation == UnityEngine.iOS.DeviceGeneration.iPhone11ProMax
+                || UnityEngine.iOS.Device.generation == UnityEngine.iOS.DeviceGeneration.iPhone11Pro
+                || UnityEngine.iOS.Device.generation == UnityEngine.iOS.DeviceGeneration.iPhone11
+                || UnityEngine.iOS.Device.generation == UnityEngine.iOS.DeviceGeneration.iPhoneX
+                || UnityEngine.iOS.Device.generation == UnityEngine.iOS.DeviceGeneration.iPhoneXR
+                || UnityEngine.iOS.Device.generation == UnityEngine.iOS.DeviceGeneration.iPhoneXS
+                || UnityEngine.iOS.Device.generation == UnityEngine.iOS.DeviceGeneration.iPhoneXSMax)
+            {
+                flattenSide = true;
+                DebugMsg.Notify("Notched Iphone Detected. Fixing Inventory UI", 3);
+            }
+        }
+        else
+        {
+            string device = SystemInfo.deviceModel;
+            foreach (string item in errorNotchDevices)
+            {
+                if(item == device) 
+                {
+                    flattenSide = true;
+                }
+            }
         }
 
+        
+        Debug.Log("DEVICE:" + UnityEngine.iOS.Device.generation.ToString());
         RefreshPanel(Screen.safeArea);
     }
 
